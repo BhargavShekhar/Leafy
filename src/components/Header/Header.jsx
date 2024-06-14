1a013ac591c3309e1c56c01f981665174a16c093
BhargavShekhar <bhargavpathak91021@gmail.com>
2024-06-14 12:51:44 +0530

    style change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -55,13 +55,13 @@ export function Header() {
   ]
 
   return (
-    <div className="relative w-full border border-black bg-white">
+    <div className="relative w-full h-20 p-3 border border-black bg-white">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
         <div className="inline-flex items-center space-x-2">
           <span>
-            <img className='h-16 object-cover object-top' src="/src/assets/logo.png" alt="reload" />
+            {/* <img className='h-16 object-cover object-top' src="/src/assets/logo.png" alt="reload" /> */}
           </span>
-          <span className="font-bold">Leafy</span>
+          <span className="font-bold text-xl">Leafy</span>
         </div>
         <div className="hidden lg:block">
           <ul className="inline-flex space-x-8">
@@ -71,7 +71,7 @@ export function Header() {
                   {/* TODO add button and onclick -> navigate function DO the same for resposive part */}
                   <Link
                     to={item.slug}
-                    className="text-sm font-bold text-gray-800 hover:text-black hover:underline"
+                    className="font-bold text-gray-800 hover:text-black hover:underline"
                   >
                     {item.name}
                   </Link>
